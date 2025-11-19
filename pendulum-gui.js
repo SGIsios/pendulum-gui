86d7db7ab6c5f908645797f67e803f9f8d0ce2c9
Sebastian Gerstberger <sebastian.gerstberger@isios.de>
2025-11-19 11:37:45 +0100

    got p-part of PID to work

--- pendulum-gui.js
+++ pendulum-gui.js
@@ -13,35 +13,7 @@ const g = 9.81; // m/s^2
 const mass = 10; // kg
 const inertia = mass * length * length;
 let running = false;
-
-function step() {
-    angularVelocity += ( -(9.8 / length) * Math.sin(angle) 
-    					 - angularVelocity * damping
-    					 + externalTorque / inertia) * dt;
-    angle += angularVelocity * dt;
-}
-
-function drawPendulum() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    const x = canvas.width/2 + lengthOnScreen * Math.sin(angle);
-    const y = canvas.height/2 + lengthOnScreen * Math.cos(angle);
-
-    // Rod
-    ctx.beginPath();
-    ctx.moveTo(canvas.width/2, canvas.height/2);
-    ctx.lineTo(x, y);
-    ctx.strokeStyle = 'red';
-    ctx.lineWidth = 4;
-    ctx.stroke();
-
-    // Bob
-    ctx.beginPath();
-    ctx.arc(x, y, 15, 0, 2*Math.PI);
-    ctx.fillStyle = 'red';
-    ctx.fill();
-    ctx.stroke();
-}
+let pidActive = false;
 
 // input: current angle
 // output: controllerTorque
@@ -50,67 +22,106 @@ const PidController = {
 	iValue: 0,
 	dValue: 0,
 	setAngle: 0,
-	setValues(p, i, d, angle) {
-		pValue = p;
-		iValue = i;
-		dValue = d;
-		setAngle = angle;
+	calcP(diff) {
+		return this.pValue * diff;
 	},
-	calcP(diff){
-		return pValue * diff;
-	},
-	calcI(diff){
+	calcI(diff) {
 		return 0;
 	},
-	calcD(diff){
+	calcD(diff) {
 		return 0;
 	},
 	calculateTorque(currentAngle) {
-		const diff = setAngle - currentAngle;
-		const torque = calcP(diff) + calcI(diff) + calcD(diff);
+		const diff = this.setAngle - currentAngle;
+		const torque = this.calcP(diff) + this.calcI(diff) + this.calcD(diff);
 		return torque;
 	}
 }
 
-function updateTorqueValue() {
+function updateValues() {
 	const externalTorqueInput = document.getElementById('external-torque');
-	externalTorque = parseFloat(externalTorqueInput.value);	
+	externalTorque = parseFloat(externalTorqueInput.value);
+	const dampingInput = document.getElementById('damping');
+	damping = parseFloat(dampingInput.value);
+	const setValueInput = document.getElementById('set-value');
+	PidController.setAngle = parseFloat(setValueInput.value);
+	const pValueInput = document.getElementById('p-value');
+	PidController.pValue = parseFloat(pValueInput.value);
+	const iValueInput = document.getElementById('i-value');
+	PidController.iValue = parseFloat(iValueInput.value);
+	const dValueInput = document.getElementById('d-value');
+	PidController.dValue = parseFloat(dValueInput.value);
+}
+
+function step() {
+	updateValues();
+	angularVelocity += (-(9.8 / length) * Math.sin(angle)
+		- angularVelocity * damping
+		+ externalTorque / inertia) * dt;
+	if (pidActive) {
+		angularVelocity += (PidController.calculateTorque(angle) / inertia) * dt;
+	}
+	angle += angularVelocity * dt;
+}
+
+function drawPendulum() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	const x = canvas.width / 2 + lengthOnScreen * Math.sin(angle);
+	const y = canvas.height / 2 + lengthOnScreen * Math.cos(angle);
+
+	// Rod
+	ctx.beginPath();
+	ctx.moveTo(canvas.width / 2, canvas.height / 2);
+	ctx.lineTo(x, y);
+	ctx.strokeStyle = 'red';
+	ctx.lineWidth = 4;
+	ctx.stroke();
+
+	// Bob
+	ctx.beginPath();
+	ctx.arc(x, y, 15, 0, 2 * Math.PI);
+	ctx.fillStyle = 'red';
+	ctx.fill();
+	ctx.stroke();
 }
 
 function setStartingValues() {
 	const angleInput = document.getElementById('start-angle');
-	const dampingInput = document.getElementById('damping');
 	angle = parseFloat(angleInput.value);
-	damping = parseFloat(dampingInput.value);
-	updateTorqueValue();
-    angularVelocity = 0;
-    drawPendulum();	
+	updateValues();
+	angularVelocity = 0;
+	drawPendulum();
 }
 
 function update() {
-    if (!running) return;
-    updateTorqueValue();
-    step();
-    drawPendulum();
-    requestAnimationFrame(update);
+	if (!running) return;
+	updateValues();
+	step();
+	drawPendulum();
+	requestAnimationFrame(update);
 }
 
 // Buttons
 document.getElementById('startBtn').onclick = () => { running = true; update(); }
 document.getElementById('stopBtn').onclick = () => { running = false; }
-document.getElementById('resetBtn').onclick = () => {setStartingValues();}
+document.getElementById('resetBtn').onclick = () => { setStartingValues(); }
 
 // show slider values
 const setValueSlider = document.getElementById("set-value");
 const setValueOut = document.getElementById("set-value-output");
 setValueSlider.addEventListener("input", () => {
-    setValueOut.textContent = setValueSlider.value;
+	setValueOut.textContent = setValueSlider.value;
 });
 
 const externalTorqueSlider = document.getElementById("external-torque");
 const externalTorqueOut = document.getElementById("external-torque-output");
 externalTorqueSlider.addEventListener("input", () => {
-    externalTorqueOut.textContent = externalTorqueSlider.value;
+	externalTorqueOut.textContent = externalTorqueSlider.value;
+});
+
+document.getElementById("activate-pid").addEventListener("change", e => {
+	pidActive = e.target.checked;
 });
 
 // Initial draw
